d5289f7e45f9fd6709db718bd0d5e8301e46bc91
ssc-vorn <sochivornseng311@gmail.com>
2026-09-07 08:05:52 +0700

    feat(builder): add admin visual builder route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CMS\BuilderController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/admin/cms/organizations/{organization}/pages/{page}/builder/{version}', [BuilderController::class, 'show'])
+        ->whereUuid(['organization', 'page', 'version'])
+        ->scopeBindings()
+        ->name('admin.cms.builder.show');
 });
 
 require __DIR__.'/auth.php';
