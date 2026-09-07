0ad7591802a9d66b6de49647d0ab071eccdd5124
ssc-vorn <sochivornseng311@gmail.com>
2026-09-07 10:02:20 +0700

    fix(auth): seed complete CMS permission matrix

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -9,31 +9,25 @@ class PermissionSeeder extends Seeder
 {
     public function run(): void
     {
-        $resources = [
-            'pages',
-            'services',
-            'project_categories',
-            'projects',
-            'blog_posts',
-            'testimonials',
-            'partners',
-            'leads',
-            'menus',
-            'menu_items',
-            'themes',
-            'settings',
-            'seo_metadata',
-            'media',
+        $permissions = [
+            'pages.view', 'pages.create', 'pages.update', 'pages.delete', 'pages.publish',
+            'services.view', 'services.create', 'services.update', 'services.delete',
+            'project_categories.view', 'project_categories.create', 'project_categories.update', 'project_categories.delete',
+            'projects.view', 'projects.create', 'projects.update', 'projects.delete', 'projects.publish',
+            'blog.view', 'blog.create', 'blog.update', 'blog.delete', 'blog.publish',
+            'testimonials.view', 'testimonials.create', 'testimonials.update', 'testimonials.delete',
+            'partners.view', 'partners.create', 'partners.update', 'partners.delete',
+            'leads.view', 'leads.create', 'leads.update', 'leads.delete',
+            'menus.view', 'menus.create', 'menus.update', 'menus.delete',
+            'menu_items.view', 'menu_items.create', 'menu_items.update', 'menu_items.delete',
+            'themes.view', 'themes.create', 'themes.update', 'themes.delete',
+            'settings.view', 'settings.create', 'settings.update', 'settings.delete',
+            'seo.view', 'seo.create', 'seo.update', 'seo.delete',
+            'media.view', 'media.create', 'media.update', 'media.delete',
         ];
 
-        $actions = ['view', 'create', 'update', 'delete'];
-
-        foreach ($resources as $resource) {
-            foreach ($actions as $action) {
-                Permission::findOrCreate("{$resource}.{$action}", 'web');
-            }
+        foreach ($permissions as $permission) {
+            Permission::findOrCreate($permission, 'web');
         }
-
-        Permission::findOrCreate('pages.publish', 'web');
     }
 }
